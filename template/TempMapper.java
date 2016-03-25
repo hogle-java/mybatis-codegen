cc6515e58a915248e88b1417171e2347092974e7
154811696 <z154811696@gmail.com>
2016-03-25 15:13:45 +0800

    Update TempMapper.java

--- template/TempMapper.java
+++ template/TempMapper.java
@@ -1,5 +1,4 @@
 /**
- * Copyright(c) 2011-2015 by UCredit Inc.
  * All Rights Reserved
  */
 package ${mapperPkg};
@@ -7,7 +6,7 @@ import java.util.Map;
 import java.util.Collection;
 import java.util.List;
 import org.springframework.stereotype.Repository;
-import com.ucredit.uudream.web.common.QueryInfo;
+import com.uudream.web.common.QueryInfo;
 import ${modelPkg}.${className};
 
 
